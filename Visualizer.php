47cc9cbf84a09b475dc22a9186ec271dd0ecbc68
porfirovskiy <porfirovskiy@gmail.com>
2019-05-05 21:03:57 +0400

    add gitignore file

--- Visualizer.php
+++ Visualizer.php
@@ -3,7 +3,7 @@
 class Visualizer {
 	
 	static public function showClassesMethods(string $className, array $methods, array $subMethods) {
-		$title = "Class $className has methods:";
+		$title = "Class $className has methods structure:";
 		echo "Class $className has methods:\n";//use log method
 		$obj = new Visualizer();
         $obj->createImage($title, $methods, $subMethods);
@@ -11,7 +11,7 @@ class Visualizer {
 	}
 	
 	public function createImage(string $title, array $methods,  array $subMethods) {
-		$image = @imagecreate(500, 1000) or die("Error");
+		$image = @imagecreate(500, 700) or die("Error");
 		$backgroundColor = imagecolorallocate($image, 0, 0, 0);
 		$textColor = imagecolorallocate($image, 233, 14, 91);
 		
@@ -19,11 +19,13 @@ class Visualizer {
 		imagestring($image, 5, 50, 5, $title, $textColor);
 		$y = 70;
 		foreach ($methods as $method) {
-			imagestring($image, 3, 5, $y, $method, $textColor);
+			$textColor = imagecolorallocate($image, 0, 102, 204);
+			imagestring($image, 3, 10, $y, $method, $textColor);
 			//render sub methods
 			if (isset($subMethods[$method])) {
 				$yy = $y + 10;
 				foreach ($subMethods[$method] as $subMethod) {
+					$textColor = imagecolorallocate($image, 0, 255, 0);
 					imagestring($image, 2, 25, $yy, $subMethod[0], $textColor);
 					$yy += 10;
 				}
